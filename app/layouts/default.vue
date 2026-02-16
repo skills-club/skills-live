1872fd21e10fa300afbb3cf748b84d386c2dc8d6
pkc918 <xiaopan2000918@gmail.com>
2026-02-16 13:29:32 +0800

    feat: add laypout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,29 @@
 <template>
-    <div>
-        312
-        <slot />
-    </div>
+  <div class="min-h-screen flex flex-col">
+    <Header
+      :show-search="showSearch"
+      :sticky="sticky"
+      :search-placeholder="searchPlaceholder"
+    />
+    <main class="flex-1 flex flex-col">
+      <slot />
+    </main>
+  </div>
 </template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const showSearch = computed(() => {
+  const value = route.meta.showSearch
+  return typeof value === 'boolean' ? value : false
+})
+const sticky = computed(() => {
+  const value = route.meta.sticky
+  return typeof value === 'boolean' ? value : false
+})
+const searchPlaceholder = computed(() => {
+  const value = route.meta.searchPlaceholder
+  return typeof value === 'string' ? value : 'Search skills,authors,org...'
+})
+</script>
